85cae740cf50d5c9f557c1348a3ae8355b92cc47
NamWoojin <nam990304@gmail.com>
2021-05-03 20:13:19 +0900

    2-6) 컴포넌트 Preloading까지

--- src/App.js
+++ src/App.js
@@ -1,35 +1,73 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Header from './components/Header'
-import InfoTable from './components/InfoTable'
-import SurveyChart from './components/SurveyChart'
-import Footer from './components/Footer'
-import ImageModal from './components/ImageModal'
+import React, { useState, useEffect, Suspense, lazy } from "react";
+import styled from "styled-components";
+import Header from "./components/Header";
+import InfoTable from "./components/InfoTable";
+import SurveyChart from "./components/SurveyChart";
+import Footer from "./components/Footer";
+// import ImageModal from "./components/ImageModal";
+
+//팩토리 패턴 이용 방식
+function lazyWithPreload(importFunction) {
+  const Component = React.lazy(importFunction);
+  Component.preload = importFunction;
+  return Component;
+}
+const LazyImageModal = lazyWithPreload(() => import("./components/ImageModal"));
+
+// const LazyImageModal = lazy(() => import("./components/ImageModal"));
 
 function App() {
-    const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
-    return (
-        <div className="App">
-            <Header />
-            <InfoTable />
-            <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
-            <SurveyChart />
-            <Footer />
-            {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
-        </div>
-    )
+  useEffect(() => {
+    // 일일이 import해주는 방식
+    // const component = import("./components/ImageModal");
+
+    //팩토리패턴 이용 방식
+    LazyImageModal.preload();
+  }, []);
+
+  // const handleMouseEnter = () => {
+  //   //마우스를 올려뒀을 때 모달 컴포넌트 불러오기
+  //   const component = import("./components/ImageModal");
+  // };
+
+  return (
+    <div className="App">
+      <Header />
+      <InfoTable />
+      <ButtonModal
+        onClick={() => {
+          setShowModal(true);
+        }}
+        // onMouseEnter={handleMouseEnter}
+      >
+        올림픽 사진 보기
+      </ButtonModal>
+      <SurveyChart />
+      <Footer />
+      <Suspense fallback={null}>
+        {showModal ? (
+          <LazyImageModal
+            closeModal={() => {
+              setShowModal(false);
+            }}
+          />
+        ) : null}
+      </Suspense>
+    </div>
+  );
 }
 
 const ButtonModal = styled.button`
-    border-radius: 30px;
-    border: 1px solid #999;
-    padding: 12px 30px;
-    background: none;
-    font-size: 1.1em;
-    color: #555;
-    outline: none;
-    cursor: pointer;
-`
+  border-radius: 30px;
+  border: 1px solid #999;
+  padding: 12px 30px;
+  background: none;
+  font-size: 1.1em;
+  color: #555;
+  outline: none;
+  cursor: pointer;
+`;
 
-export default App
+export default App;
